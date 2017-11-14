2848f9f9e7d661a924f7ea838daaa169b5f3dd6b
Filipe Boaventura <fmbboaventura@gmail.com>
2017-11-14 01:04:03 -0300

    Implementação da equação dz e adição da flag -fg no Makefile

--- serial/main.c
+++ serial/main.c
@@ -16,8 +16,9 @@ double getRealTime(){
 
 double x=0, y=0, z=0, xl0=0, yl0=0, zl0=0;
 
+double vZ(int t, double X, double gama, double vez, double H, double I);
+
 int main(int argc, char *argv[]) {
-    printf("Hello\n");
     //Start time
 	time(&timer1);
 	tm_info = localtime(&timer1);
@@ -25,50 +26,43 @@ int main(int argc, char *argv[]) {
 	startTime = getRealTime();
 
     int NPI = atoi(argv[1]); // numero de posicoes iniciais
+    double velociade_final[15999]; // Vetor das posicoes finais pra cada linha
     FILE *arq, *out;
     char url[] = "in.dat";
     arq = fopen(url, "r");
     out = fopen("parallel-out.txt", "w");
     double var1;
 
-    //printf("Numero de posicoes iniciais: %d\n", NPI);
-
     for(int np = 1; np <= NPI; np++) {
-        //printf("Problema %d\n", np);
         if(arq == NULL) {
             printf("Erro, nao foi possivel abrir o arquivo\n");
             exit(EXIT_FAILURE);
         } else {
-            fscanf(arq,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n", &var1, &var1, &var1, &x, &y, &z, &var1, &xl0, &yl0, &zl0, &var1, &var1, &var1, &var1, &var1, &var1, &var1, &var1, &var1);
-            //printf("%lf %lf %lf %lf %lf %lf\n", x, y, z, xl0, yl0, zl0);
+            fscanf(arq,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
+                &var1, &var1, &var1, &x, &y, &z, &var1, &xl0, &yl0, &zl0, &var1, &var1, &var1, &var1, &var1,
+                &var1, &var1, &var1, &var1);
         }
-        //#pragma omp parallel for
-        for(double Ve = 0.5; Ve<=0.5; Ve+=0.5) {
-            //printf("Ve %d\n", VeAux);
+
+        //int j = 0;
+        for(double Ve = 0.5; Ve<=5; Ve+=0.5) {
             double vex, vey, vez;
             vex = vey = vez =Ve*Ve/3;
-            //#pragma omp parallel for
-            for(int aux = 0; aux<=0; aux++){
-                //printf("Gama %d\n", aux);
-                double gama = pow(10, aux);
-                //int tid = omp_get_thread_num();
-                //printf("Hello world from omp thread %d\n", tid);
-                //#pragma omp parallel for firstprivate(z, x, y, zl0, xl0, yl0)
-                for(int Xaux=1; Xaux<=1; Xaux++) {
-                    //printf("X %d\n", Xaux);
-                    double X = Xaux;
 
+            for(int aux = -14; aux<=2; aux++){
+                double gama = pow(10, aux);
+
+                for(int Xaux=1; Xaux<=100; Xaux++) {
+                    double X = Xaux;
                     double H = brute_H (z, gama, vex);
                     double I = brute_I (zl0, gama, X, vez);
+                    double dz = 0;
 
-                    printf("H:%lf \nI:%lf \n", H, I);
-                    //nave = nave + 1;
-                    //int ID = omp_get_thread_num();
-                    //printf("Simulando nave %.1f\n", nave);
-                    //#pragma omp parallel for
-                    for(int t = 1; t <= Tmax; t++) {
-
+                    for(int t = 0; t <= Tmax; t++) {
+                        dz = vZ(t, X, gama, vez, H, I);
                     }
+                    //velociade_final[j] = dz;
+                    //j++;
+                    //printf("H:%lf \nI:%lf \nvZ:%lf\n", H, I, dz);
                 }
             }
         }
@@ -78,7 +72,34 @@ int main(int argc, char *argv[]) {
     strftime(buffer2, 25, "%d/%m/%Y %H:%M:%S", tm_info);
 
     finalTime = getRealTime();
-	fprintf(out, "Tempo em segundos: %lf", finalTime);
+	fprintf(out, "Tempo total de execucao (s): %lf", finalTime-startTime);
+    printf("Tempo total de execucao (s): %lf", finalTime-startTime);
     fclose(out);
     return 0;
+}
+
+double vZ(int t, double X, double gama,  double vez, double H, double I) {
+    //otimizacao
+    double wt = w*t;
+
+    double resultJn = 0;
+    double result1 = (-H)*w*sin(wt)+I*w*cos(wt);
+    double result2 = 0;
+
+    //otimizacao
+    double gama_wpow = (gama/w)*(gama/w);
+    double gamat = gama*t;
+
+    for (int n = 1; n <= N; n++) {
+        //brute_J
+        resultJn = vez/(n*pow(X,n)*w)/(1+(n*n*gama_wpow));
+        if (n%2 == 0) {
+            resultJn = -resultJn;
+        }
+        //brute_J
+
+        result2 += resultJn*((-n)*gama*pow(M_E, -(n*gamat)));
+    }
+
+    return result1  - result2;
 }
