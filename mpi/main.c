737a7083151fdc9786fadd7e6e770eb967902d87
Filipe Boaventura <fmbboaventura@gmail.com>
2017-12-18 22:57:41 -0300

    Teste da leitura do arquivo de entrada no cluster

--- mpi/main.c
+++ mpi/main.c
@@ -2,46 +2,75 @@
 #include <stdlib.h>
 #include "const.h"
 #include <mpi.h>
-#include <string.h>
 
 extern double wtime();
 
+double x=0, y=0, z=0, xl0=0, yl0=0, zl0=0;
+
+double vZ(int t, double X, double gama, double vez, double H, double I);
+
 int main(int argc, char* argv[]){
-	int  my_rank; /* rank of process */
-	int  p;       /* number of processes */
-	int source;   /* rank of sender */
-	int dest;     /* rank of receiver */
-	int tag=0;    /* tag for messages */
-	char message[100];        /* storage for message */
-	MPI_Status status ;   /* return status for receive */
+	int  my_rank; /* rank do processo */
+	int  p;       /* numero de processos */
+	int source;   /* rank do sender */
+	int dest;     /* rank do receiver */
+	int tag=0;    /* tag para messagens */
+	char message[100];        /* mensagem */
+	MPI_Status status ;
 
-	/* start up MPI */
+	int NPI = atoi(argv[1]); // numero de posicoes iniciais
+	FILE *arq;
+    char url[] = "in.dat";
+    double var1;
 
+	arq = fopen(url, "r");
+	if(arq == NULL) {
+		printf("Erro, nao foi possivel abrir o arquivo\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Inicializa o MPI */
 	MPI_Init(&argc, &argv);
 
-	/* find out process rank */
+	/* pega o process rank */
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-	/* find out number of processes */
+	/* pega o numero de processos */
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
+	for(int np = 1; np <= NPI; np++) {
 
-	if (my_rank !=0){
-		/* create message */
-		sprintf(message, "Hello MPI World from process %d!", my_rank);
-		dest = 0;
-		/* use strlen+1 so that '\0' get transmitted */
-		MPI_Send(message, strlen(message)+1, MPI_CHAR,
-		   dest, tag, MPI_COMM_WORLD);
-	}
-	else{
-		printf("Hello MPI World From process 0: Num processes: %d\n",p);
-		for (source = 1; source < p; source++) {
-			MPI_Recv(message, 100, MPI_CHAR, source, tag,
-			      MPI_COMM_WORLD, &status);
-			printf("%s\n",message);
+		fscanf(arq,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
+			&var1, &var1, &var1, &x, &y, &z, &var1, &xl0, &yl0, &zl0, &var1, &var1, &var1, &var1, &var1,
+			&var1, &var1, &var1, &var1);
+
+		switch (my_rank) {
+			case 0: printf("Rank: %d z: %f dz: %f\n", my_rank, z, zl0);break;
+			case 0: printf("Rank: %d z: %f dz: %f\n", my_rank, z, zl0);break;
+			case 0: printf("Rank: %d z: %f dz: %f\n", my_rank, z, zl0);break;
+			case 0: printf("Rank: %d z: %f dz: %f\n", my_rank, z, zl0);break;
 		}
 	}
+
+	// if (my_rank !=0){
+	// 	/* create message */
+	// 	sprintf(message, "Hello MPI World from process %d!", my_rank);
+	// 	dest = 0;
+	// 	/* use strlen+1 so that '\0' get transmitted */
+	// 	MPI_Send(message, strlen(message)+1, MPI_CHAR,
+	// 	   dest, tag, MPI_COMM_WORLD);
+    //     system("cat /proc/cpuinfo");
+	// }
+	// else{
+	// 	printf("Hello MPI World From process 0: Num processes: %d\n",p);
+	// 	for (source = 1; source < p; source++) {
+	// 		MPI_Recv(message, 100, MPI_CHAR, source, tag,
+	// 		      MPI_COMM_WORLD, &status);
+	// 		printf("%s\n",message);
+    //         system("cat /proc/cpuinfo");
+	// 	}
+	// }
+
 	/* shut down MPI */
 	MPI_Finalize();
 
